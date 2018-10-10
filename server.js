9bc0e279d0e4e3b655ea403b94fe0fac318d101f
Liz Steak <lizmaepark@gmail.com>
2018-10-10 11:17:20 -0600

    index route creatd

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express;
+const express = require("express");
+const app = express();
 port = 3000;
 
 const Wod = require('./wod');
@@ -7,17 +7,15 @@ const Wod = require('./wod');
 
 // Come up with a CRUDable model (fruit, human, robot, etc)
 
-// Create an array of your CRUDable objects. Commit.
 // Create an index route that displays all your objects. Commit.
 // In the index route, create a form which will make a DELETE request to the server. Commit.
 // Create a DELETE route handler that will remove the specified object from the array and redirect to the index page. Commit.
 
 
-app.get('/', (req, res) => {
+app.get('/workout', (req, res) => {
     console.log('hi there')
-    // res.render('wod.js', {workout: Wod})
+    res.render('index.ejs', {workout: Wod})
 });
-
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
