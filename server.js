2fdcfc25630a08dd815206420acf73d1b533f820
Liz Steak <lizmaepark@gmail.com>
2018-10-10 11:56:34 -0600

    delete form completed

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 port = 3000;
-
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const Wod = require('./wod');
 
-
-// Come up with a CRUDable model (fruit, human, robot, etc)
-
-// Create an index route that displays all your objects. Commit.
+app.use(bodyParser.urlencoded());
+app.use(methodOverride('_method'));
 // In the index route, create a form which will make a DELETE request to the server. Commit.
 // Create a DELETE route handler that will remove the specified object from the array and redirect to the index page. Commit.
 
@@ -17,8 +16,26 @@ app.get('/workout', (req, res) => {
     res.render('index.ejs', {workout: Wod})
 });
 
+app.get('/workout/:id', (req, res) => {
+    res.render('show.ejs', {
+        workout: Wod[req.params.id]
+    })
+})
+
+app.delete('/workout/:id', (req, res) => {
+    Wod.splice(req.params.index, 1);
+    res.redirect('/workout')
+})
+
 app.listen(3000, () => {
     console.log('listening on port 3000')
 });
 
 module.exports = app;
+
+{/* <form action="/workout method="POST">
+<li>Name: <input type="text" name="name"></li> 
+<li>Weight: <input type="text" name="weight"></li>
+<li>Type: <input type="text" name="type"></li>
+<li><input type="submit" name="" value="Delete"/></li>
+</form> */}
