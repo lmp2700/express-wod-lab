e6e0a09ff93b3886159f3a8ff887188923ad6b75
Liz Steak <lizmaepark@gmail.com>
2018-10-10 11:08:17 -0600

    added .gitiignore

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express')
-port = 3000
+const express = require('express');
+const app = express;
+port = 3000;
 
-const Wod = require('./wod.js')
+const Wod = require('./wod');
 
 
 // Come up with a CRUDable model (fruit, human, robot, etc)
 
-// Set up a repo.
-// Create an express app. .gitignore your node_modules. Commit.
 // Create an array of your CRUDable objects. Commit.
 // Create an index route that displays all your objects. Commit.
 // In the index route, create a form which will make a DELETE request to the server. Commit.
 // Create a DELETE route handler that will remove the specified object from the array and redirect to the index page. Commit.
 
 
-app.get('/workout', (req, res) => {
-    res.render('wod.js')
+app.get('/', (req, res) => {
+    console.log('hi there')
+    // res.render('wod.js', {workout: Wod})
 });
 
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
 });
+
+module.exports = app;
